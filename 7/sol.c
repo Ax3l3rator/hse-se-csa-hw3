103131df3c5b303581425fe8e824d4275a7e46ca
Ax3l3rator <ax3l3rator@d00m-m4ch1n3.localdomain>
2022-11-27 23:37:28 +0300

    UwU

--- 7/sol.c
+++ 7/sol.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
-double findSum(register double x) {
-    register double e = 1;
-    register double prev = 1;
-    register double val = 0;
+double findSum(double x) {
+    double e = 1;
+    double prev = 1;
+    double val = 0;
     for (register int i = 1; i <= 100; ++i) {
         val = prev * x / i;
         e += val;
@@ -14,6 +15,11 @@ double findSum(register double x) {
 }
 
 int main(int argc, char const *argv[]) {
-    /* code */
+    if (argc < 4) {
+        printf("Args count???");
+        return 1;
+    }
+    if (!strcmp(argv[1], "--file")) {
+    }
     return 0;
 }
